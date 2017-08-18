a190857ab7d2fef89e81edbb3c34b7957bb4b493
RAGHURAM <RAGHURAM@DESKTOP-KNCDMSJ>
2017-08-18 16:44:13 +0530

    front end

---
+++ mobilesfrontend/src/main/java/com/niit/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.niit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+
+public class HomeController {
+	@RequestMapping("/home")
+	public String homepage() {
+		return "home";
+		
+	}
+	
+	@RequestMapping("/aboutus")
+	public String aboutUs() {
+		return "aboutus";
+	}
+}
